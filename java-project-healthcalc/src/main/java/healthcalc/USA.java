de111851e7cdd8cc89faf4a03789c8848ab2328c
Ana <anna.pahontu@gmail.com>
2026-05-17 13:50:54 +0200

    fix: Arreglar patrón Decorator en region

--- java-project-healthcalc/src/main/java/healthcalc/USA.java
+++ java-project-healthcalc/src/main/java/healthcalc/USA.java
@@ -7,20 +7,17 @@ public class USA extends BaseDecoratorRegion {
 
     @Override
     public Tuple<Float, String> indiceMasaCorporal(float heightPulgadas, int weightLibras) {
-        // Conversión según el enunciado
-        float alturaMetros = (float) (heightPulgadas * 0.0254);// 1 libra = 0.453592 kg
-        int pesoKg = (int) (weightLibras / 2.2046);  // 1 pulgada = 0.0254 metros
+        float alturaMetros = (float) (heightPulgadas * 0.0254);
+        int pesoGramos = (int) (weightLibras * 453.592);
 
-        Tuple<Float, String> result = super.indiceMasaCorporal(alturaMetros, pesoKg);
-        float bmi = result.getFirst();
-        String classification = result.getSecond();
-
-        return new Tuple<>(bmi, classification);
+        return super.indiceMasaCorporal(alturaMetros, pesoGramos);
     }
 
     @Override
-    public int pesoCorporalIdeal(char genero, float altura) {
-        float idealWeight = super.pesoCorporalIdeal(genero, altura);
-        return (int) (idealWeight * 2.2046); // Convertir de kg a libras
+    public int pesoCorporalIdeal(char genero, float heightPulgadas) {
+        float alturaMetros = (float) (heightPulgadas * 0.0254);
+        int idealWeightKg = super.pesoCorporalIdeal(genero, alturaMetros);
+        
+        return (int) (idealWeightKg * 2.2046); 
     }
 }
